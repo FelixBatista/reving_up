3c591e1f3f5e12c96b13a8e16848115be988a08b
FelixBatista <felixb.neto@gmail.com>
2024-10-08 21:30:46 +0200

    Added todo to can_bus_service.dart

--- lib/can_bus_service.dart
+++ lib/can_bus_service.dart
@@ -1,3 +1,15 @@
+/*
+Steps:
+1.Create Native Modules:
+  Implement CAN bus communication in Kotlin/Java using existing libraries or the code from TeyesCanBusGolf7RZC.
+
+2.Expose Methods to Dart:
+  Use platform channels to send and receive RPM data.
+
+3.Handle Permissions:
+  Ensure the app has necessary permissions to access CAN bus interfaces.
+ */
+
 // can_bus_service.dart
 import 'package:flutter/services.dart';
 
